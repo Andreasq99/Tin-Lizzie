676cfcd4652e42862c052aee99901d34cd79cee8
ajmecca68 <137560371+ajmecca68@users.noreply.github.com>
2023-11-06 22:08:37 -0500

    solved the problem with login not carrying over to other pages

--- controllers/api/auth.js
+++ controllers/api/auth.js
@@ -34,9 +34,6 @@ router.post(
         return res.status(400).json({ message: 'Email already registered' });
       }
 
-      // Hash the password
-      const hashedPassword = await bcrypt.hash(password, 10); 
-
       // Create a new user with first_name, last_name, email, and password
       const newUser = await User.create({
         first_name,
