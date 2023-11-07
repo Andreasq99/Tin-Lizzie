1f84db894731d17c3e6cc9661cdce612ea6efed3
Collier Cobb <collierj.cobb@gmail.com>
2023-11-06 21:13:30 -0500

    vehicle registration?

--- public/js/vehicleRegistration.js
+++ public/js/vehicleRegistration.js
@@ -1,8 +1,9 @@
 async function registrationHandler(event) {
   event.preventDefault();
 
-  const vehicleMake = document.querySelector("#vehicle-make").value;
-  const vehicleModel = document.querySelector("#vehicle-model").value;
+  const make = document.querySelector("#vehicle-make").value;
+  const model = document.querySelector("#vehicle-model").value;
+  const type = document.querySelector("#type").value;
   const year = document.querySelector("#year").value;
   const color = document.querySelector("#color").value;
   const vin = document.querySelector("#vin").value;
@@ -11,9 +12,28 @@ async function registrationHandler(event) {
   const mileage = document.querySelector("#mileage").value;
   const description = document.querySelector("#description").value;
 
+  const userId = req.session.userId;
+  console.log(userId);
+
   try {
-    const response = await fetch("/api/auth", {
+    const response = await fetch("/api/vehicles", {
       method: "POST",
+      body: JSON.stringify({
+        userId,
+        type,
+        make,
+        model,
+        year,
+        color,
+        vin,
+        price,
+        condition,
+        mileage,
+        description,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
     
     if (response.ok) {
